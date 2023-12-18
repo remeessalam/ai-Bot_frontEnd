337524d6bce92c95b2c4d9b30d49144c6294fca1
remeessalam <remeesreme4u@gmail.com>
2023-12-18 12:16:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+const App = () => {
+  const [text, setText] = useState("");
+  const [answer, setAnswer] = useState("");
+  const apiCall = async () => {
+    try {
+      const response = await fetch("http://localhost:3000", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          Promt: text,
+        }),
+      });
 
-function App() {
+      const result = await response.json();
+      const newText = result?.result.replace(/[|&;$%@"<>()+,`'*]/g, "");
+
+      setAnswer(newText);
+    } catch (error) {
+      console.log(error, "This is error");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="page-container">
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => {
+            setText(e?.target?.value);
+          }}
+        />
+
+        <button
+          onClick={() => {
+            apiCall();
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Get a API call
+        </button>
+        <p>{answer}</p>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
